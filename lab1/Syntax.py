7f44ec985d96d652e1fcc5c20d64ecf5bcc35813
Alikhan <alikhannurlanuly18@gmail.com>
2024-01-26 19:01:00 +0600

    lab1syntax

--- lab1/Syntax.py
+++ lab1/Syntax.py
@@ -1,2 +1,19 @@
+#Example1
+#Python uses indentation to indicate a block of code.
 if 5 > 2:
     print("Five is greater than two!")
+#Example2
+#The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!")  
+#Example3
+#Variables in Python:
+x = 5
+y = "Hello, World!"
+#Example4
+#This is a comment.
+print("Hello, World!")
+#Exercise:
+print("Hello World!")
