406bd16f2bf185384c62cc48689daff1a4fe95af
Alikhan <alikhannurlanuly18@gmail.com>
2024-01-26 20:00:13 +0600

    change

--- lab1/HOME.py
+++ lab1/HOME.py
@@ -2,3 +2,6 @@
 print("Hello, World!")
 #Exercise1
 print("Hello World!")
+#Exercise2
+if 5 > 2:
+    print("YES")
---
+++ lab1/Variable_Exercise.py
@@ -0,0 +1,37 @@
+#Exercises
+#1
+carname = "Volvo"
+#2
+x = 50
+#3
+x = 5
+y = 10 
+print(x+y)
+#4
+x = 5
+y = 10 
+z = x+y
+print(Z)
+#5
+x,y,z = "Orange", "Banana", "Cherry"
+#6
+x = y = z = "Orange"
+def myfunc():
+    global  x
+    x = "fantastic"
+#7
+x = 5
+print(type(x))
+#Answer: data type would be int
+#7
+x = "Hello World"
+print(type(x))
+#Answer: data type would be str
+#8
+x = 20.5
+print(type(x))
+#Answer: data type would be float
+#9
+x = ["apple", "banana", "cherry"]
+print(type(x))
+#Answer: data type would be list
